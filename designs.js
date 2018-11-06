49eaa7ff3fd6a35ab5641200efd21bbc0ca9c278
Pinguz <micky.zjj@gmail.com>
2018-11-06 15:01:41 +0800

    Add interaction

--- designs.js
+++ designs.js
@@ -1,10 +1,37 @@
 // Select color input
 // Select size input
+let canvas = document.getElementById("pixelCanvas");
+let height = document.getElementById("inputHeight");
+let weight = document.getElementById("inputWidth");
+let sizePicker = document.getElementById("sizePicker");
+let color = document.getElementById("colorPicker");
+
+color.addEventListener("click",function(){});
+
+sizePicker.onsubimt = function(event){
+  event.preventDefault();
+  clearGrid();
+  makeGrid();
+};
 
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
+  for (let r=0;r<height.value;r++){
+    const row = canvas.insertRow(r);
+    for(let c=0; c<weight.value;c++){
+      const cell = row.insertCell(c);
+      cell.addEventListener("click",fillSquare);
+    }
+  }
+}
 
-// Your code goes here!
+function clearGrid(){
+  while (canvas.firstChild){
+    canvas.removeChild(canvan.firstChild);
+  }
+}
 
+function fillSquare(){
+  this.setAttribute("style",'background-color':${color.value});
 }
